524d2c8fab6c43c40325c168cb55f11f2a4374bb
Kinrezc <matt.czernik@gmail.com>
2019-12-02 13:28:37 -0500

    fixing web3 modal init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/styles";
 import {
   Typography,
@@ -10,10 +10,9 @@ import {
 } from "@material-ui/core";
 import Web3 from "web3";
 import Portis from "@portis/web3";
-import { TerminalHttpProvider, SourceTypes } from "@terminal-packages/sdk";
+import { TerminalHttpProvider, SourceType } from "@terminal-packages/sdk";
 
 const portis = new Portis("process.env.portis", "mainnet");
-
 const apiKey = process.env.apiKey;
 const projectId = process.env.projectId;
 
@@ -23,7 +22,9 @@ const defaultObject = {
 };
 
 const portisObject = {
-  ...defaultObject
+  ...defaultObject,
+  customHttpProvider: portis.provider,
+  source: SourceType.Portis
 };
 
 const useStyles = makeStyles({
@@ -42,25 +43,43 @@ const useStyles = makeStyles({
     maxHeight: 600,
     width: 450,
     height: 600
+  },
+  web3ButtonWrapper: {
+    marginTop: 50
   }
 });
 
-const initWeb3 = type => {
+const initWeb3 = (type, setWeb3) => {
   switch (type) {
     case "metamask":
-      window.ethereum.enable.then(() => new Web3(window.terminal.ethereum));
+      !window.ethereum
+        ? window.ethereum.enable.then(() =>
+            setWeb3(new Web3(window.terminal.ethereum))
+          )
+        : setWeb3(new Web3(window.terminal.ethereum));
+      break;
     case "portis":
-      return new Web3(new TerminalHttpProvider(portisObject));
+      setWeb3(Web3(new TerminalHttpProvider(portisObject)));
+      break;
+    default:
+      throw new Error("Invalid web3 option");
   }
 };
 
-const InitWeb3Button = ({ type, name, classes }) => (
-  <Button className={classes.web3Button} onClick={() => initWeb3(type)}>
-    {name}
-  </Button>
+const InitWeb3Button = ({ type, name, classes, setWeb3 }) => (
+  <div className={classes.web3ButtonWrapper}>
+    <Button
+      className={classes.web3Button}
+      onClick={() => {
+        initWeb3(type, setWeb3);
+      }}
+    >
+      {name}
+    </Button>
+  </div>
 );
 
-const Web3Modal = ({ open, handleClose, classes }) => {
+const Web3Modal = ({ open, handleClose, classes, setWeb3 }) => {
   return (
     <Dialog
       open={open}
@@ -74,6 +93,18 @@ const Web3Modal = ({ open, handleClose, classes }) => {
           <Typography variant="h4" style={{ marginTop: 50 }}>
             Select Web3 Provider
           </Typography>
+          <InitWeb3Button
+            type="metamask"
+            name="metamask"
+            classes={classes}
+            setWeb3={setWeb3}
+          />
+          <InitWeb3Button
+            type="portis"
+            name="portis"
+            classes={classes}
+            setWeb3={setWeb3}
+          />
         </Paper>
       </Fade>
     </Dialog>
@@ -83,6 +114,7 @@ const Web3Modal = ({ open, handleClose, classes }) => {
 const App = () => {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
+  const [web3, setWeb3] = useState(null);
 
   const handleOpen = () => {
     setOpen(true);
@@ -99,7 +131,22 @@ const App = () => {
           OPEN MODAL
         </Button>
       </div>
-      <Web3Modal open={open} handleClose={handleClose} classes={classes} />
+      <div style={{ marginTop: 45 }}>
+        <Button
+          variant="contained"
+          color="primary"
+          disabled={!web3}
+          onClick={() => web3.eth.getBlockNumber()}
+        >
+          Get Block Number
+        </Button>
+      </div>
+      <Web3Modal
+        open={open}
+        handleClose={handleClose}
+        classes={classes}
+        setWeb3={setWeb3}
+      />
     </div>
   );
 };
